a3ea01dfe31db0415de3af02c81ffa4eb0a7c584
olofer <k.e.j.olofsson@gmail.com>
2023-05-04 16:26:56 +0200

    internally wraparound source phase (when applicable)

--- fdtd-constants.hpp
+++ fdtd-constants.hpp
@@ -5,3 +5,4 @@ const double vacuum_permittivity = 8.854187812813 * 1.0e-12; // [F / m]
 const double vacuum_impedance = std::sqrt(vacuum_permeability / vacuum_permittivity);
 const double vacuum_velocity = 1.0 / std::sqrt(vacuum_permeability * vacuum_permittivity);
 const double courant_factor = 1.0 / std::sqrt(2.0);
+const double two_pi = 2.0 * M_PI;
--- fdtd-source.hpp
+++ fdtd-source.hpp
@@ -20,6 +20,7 @@ struct fdtdSource
   double delayMultiplier;
   double theta;
   double radiansPerTimestep;
+  bool thetaWraparound;
 
   void initDefault() {
     type = fdtdSourceType::Monochromatic;
@@ -28,6 +29,7 @@ struct fdtdSource
     y = 0.05;
     amp = 1.0;
     delayMultiplier = 2.0;
+    thetaWraparound = true;
     resetTheta();
     setPPW(30.0);
   }
@@ -52,7 +54,7 @@ struct fdtdSource
     return (-1 + 2.0 * static_cast<double>(q % Q) / Q) * (this->amp);
   }
 
-  double soft_sawtooth() const {
+  double truncated_sawtooth() const {
     // (sin(x) - 1/2 sin(2 x) + 1/3 sin(3 x) - 1/4 sin(4 x) + 1/5 sin(5 x)) * (2 / pi)
     const double multiplier = (2.0 / M_PI) * amp;
     double s = std::sin(theta);
@@ -63,7 +65,7 @@ struct fdtdSource
     return s * multiplier;
   }
 
-  double soft_squarewave() const {
+  double truncated_squarewave() const {
     const double multiplier = (4.0 / M_PI) * amp;
     double s = std::sin(theta);
     s += std::sin(3.0 * theta) / 3.0;
@@ -74,19 +76,24 @@ struct fdtdSource
 
   void off() {
     type = fdtdSourceType::NoSource;
+    resetTheta();
   }
 
   void setPPW(int ppw) {
     this->ppw = ppw;
-    this->radiansPerTimestep = 2.0 * M_PI * courant_factor / this->ppw;
+    this->radiansPerTimestep = two_pi * courant_factor / this->ppw;
   }
 
   void updateTheta() {
-    this->theta += this->radiansPerTimestep;
+    theta += radiansPerTimestep;
+    if (thetaWraparound) {
+      if (theta > M_PI) theta -= two_pi;
+      if (theta < -M_PI) theta += two_pi;
+    }
   }
 
   void resetTheta() {
-    this->theta = 0.0;
+    theta = 0.0;
   }
 
   // Compute conductivity (sigma) times space-step (delta) to achieve a skin-length of lhat space steps
@@ -115,12 +122,12 @@ struct fdtdSource
     case fdtdSourceType::SquareWave:
       //Sxy = (sinusoidal(static_cast<double>(counter)) < 0.0 ? -this->amp : this->amp);
       //Sxy = (sinusoidal() < 0.0 ? -this->amp : this->amp);
-      Sxy = soft_squarewave();
+      Sxy = truncated_squarewave();
       break;
 
     case fdtdSourceType::Sawtooth:
       //Sxy = sawtooth(counter);
-      Sxy = soft_sawtooth();
+      Sxy = truncated_sawtooth();
       break;
 
     case fdtdSourceType::NoSource:
